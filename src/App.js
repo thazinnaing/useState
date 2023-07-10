b840ade5f406af0252e04b7023ec57b1febc4628
thazin naing <thazinaingmm@gmail.com>
2023-07-10 17:40:55 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {sculptureList} from "./components/data.js"
+
 
 function App() {
+  const [index,setIndex]=useState(0);
+  const [showMore, setshowMore]=useState(false);
+
+  function handleClick() {
+    setIndex(index+1);
+
+  }
+  function handleMoreClick(){
+    setshowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <button className="nextBtn" onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+
+      <button className="detailBtn" onClick={handleMoreClick}>
+        {showMore? "Hide": "Show"} details
+      </button>
+
+      {showMore && <p>{sculpture.description}</p>}
+
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
     </div>
   );
 }
